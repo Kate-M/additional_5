866bd66485e17f41290a429a40020e88d0cadd67
kate <kate@openledger.info>
2017-09-26 13:01:31 +0300

    done brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  function isPair(bracketsConfig, cOpen, cClosed) {
+    for (var i = 0; i < bracketsConfig.length; i++) {
+      if (cOpen == bracketsConfig[i][0] && cClosed == bracketsConfig[i][1])
+        return true;
+    }
+    return false;
+  }
+
+  function isOpen(bracketsConfig, c) {
+    for (var i = 0; i < bracketsConfig.length; i++) {
+      if (c == bracketsConfig[i][0]) return true;
+    }
+    return false;
+  }
+
+  function isClosed(bracketsConfig, c) {
+    for (var i = 0; i < bracketsConfig.length; i++) {
+      if (c == bracketsConfig[i][1]) return true;
+    }
+    return false;
+  }
+
+  var arr = str.split("");
+  var newBrackets = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (isOpen(bracketsConfig, arr[i]) && isClosed(bracketsConfig, arr[i])) {
+      var lastbracket = newBrackets.pop();
+      if (lastbracket != arr[i]) {
+        if (lastbracket)
+          newBrackets.push(lastbracket);
+        newBrackets.push(arr[i]);
+      }
+      continue;
+    }
+
+    if (isOpen(bracketsConfig, arr[i])) {
+      newBrackets.push(arr[i]);
+    }
+
+    if (isClosed(bracketsConfig, arr[i])) {
+      var x = newBrackets.pop();
+
+      if (!isPair(bracketsConfig, x, arr[i]))
+        return false;
+    }
+  }
+  return newBrackets.length == 0;
+
+
 }
